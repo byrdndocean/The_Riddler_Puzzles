faaf30dc4c238b2496466c61fa3e629a67dd4b80
Dresden Foster <byrdndocean@gmail.com>
2017-09-18 07:10:25 -0400

    fixed javascript, missing quotes

--- Riddler.js
+++ Riddler.js
@@ -2,7 +2,7 @@ function numberGuess() {
 	var randomNumber = Math.floor((Math.random() * 10) + 1);
 	var yourGuess = document.getElementById("guess");
 	
-	if (yourGuess.value === randomNumber) {
+	if (yourGuess.value == randomNumber) {
 		document.getElementById("prompt").innerHTML ="That is......Correct";
 		}
 	else {
@@ -12,9 +12,9 @@ function numberGuess() {
 
 function lastName() {
 	var lname = "Nigma";
-	var yourGuess1 = document.getElementById("guess1");
+	var yourGuess = document.getElementById("guess1");
 	
-	if(yourGuess1.value === lname) {
+	if(yourGuess.value == lname) {
 		document.getElementById("prompt1").innerHTML ="That is....Correct";
 	}else{
 		document.getElementById("prompt1").innerHTML ="That is.....InCorrect, Think Harder";
